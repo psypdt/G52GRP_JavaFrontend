076f8b9a03c6795764f6120139f49879939a16a0
Philipp <psypdt@nottingham.ac.uk>
2019-01-29 13:11:27 +0000

    Created an interface for the website loader class, added junit tests WebPageLoader class

--- JavaFrontend_Project/UnitTests/sample/functionality/webpage/WebPageLoaderTest.java
+++ JavaFrontend_Project/UnitTests/sample/functionality/webpage/WebPageLoaderTest.java
@@ -1,8 +1,38 @@
 package sample.functionality.webpage;
 
+import org.junit.Before;
+import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class WebPageLoaderTest
 {
+    WebPageLoader pageLoader = new WebPageLoader();
 
+
+    @Test
+    /**
+     * This method will test a connection to the website is made
+     */
+    void test_site_connection()
+    {
+
+    }
+
+    @Test
+    public void start()
+    {
+
+    }
+
+    @Test
+    public void load_webpage()
+    {
+
+    }
+
+    @Test
+    public void reload_web_page()
+    {
+
+    }
 }
--- JavaFrontend_Project/src/sample/functionality/webpage/WebPageLoader.java
+++ JavaFrontend_Project/src/sample/functionality/webpage/WebPageLoader.java
@@ -1,7 +1,53 @@
 package sample.functionality.webpage;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class WebPageLoader
+import java.net.URL;
+
+
+/**
+ * Load information from webpage
+ */
+public class WebPageLoader extends Application implements WebPageLoaderInterface
 {
+    private URL homeURL;
+
+
+    /**
+     * @param homePage This is the default website that will be loaded when opening the app
+     */
+    public WebPageLoader(String homePage)
+    {
+
+    }
+
+    /**
+     * This function will be responsible for displaying the webpage
+     * @param primaryStage
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+
+    }
+
+    /**
+     * @param url This is the url string that will be loaded
+     */
+    @Override
+    public void load_webpage(String url)
+    {
+
+    }
+
+    /**
+     *
+     * */
+    @Override
+    public void reload_web_page()
+    {
+
+    }
 
 }
