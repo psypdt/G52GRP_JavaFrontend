398bf5e276ce1892f69561cfebd1cba876a2d0e3
Philipp <psypdt@nottingham.ac.uk>
2019-01-31 15:03:43 +0000

    Added a static function getStage() to main so that the mainStage is accessible from anywhere Authors: Philipp Tiso

--- JavaFrontend_Project/src/sample/Main.java
+++ JavaFrontend_Project/src/sample/Main.java
@@ -8,10 +8,19 @@ import javafx.stage.Stage;
 
 public class Main extends Application
 {
+    private static Stage mainStage;
+
+    /*This function allows us to get the main stage from anywhere by calling Main.getStage()*/
+    public static Stage getStage()
+    {
+        return mainStage;
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception
     {
+        mainStage = primaryStage;
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
