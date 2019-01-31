c01290177a9e1b35bb05a55ce47204c50600f4d6
Philipp <psypdt@nottingham.ac.uk>
2019-01-31 15:11:39 +0000

    Added JUnit tests for the Main method and its controller Authors: Philipp Tiso

---
+++ JavaFrontend_Project/UnitTests/sample/runApplication/MainTest.java
@@ -0,0 +1,27 @@
+package sample.runApplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest
+{
+
+    @Test
+    public void getStage()
+    {
+
+    }
+
+    @Test
+    public void start()
+    {
+
+    }
+
+    @Test
+    public void main()
+    {
+
+    }
+}
