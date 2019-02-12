eaa071917781735b5312deb01cb5f72811c2b436
Brendan Lum <psybylu@exmail.nottingham.ac.uk>
2019-02-12 13:11:23 +0000

    Delete default files
    - Delete the default Controller and FXML files
    - Move the Main's Controller file to be in the same package

--- JavaFrontend_Project/src/sample/runApplication/Controller.java
+++ JavaFrontend_Project/src/sample/runApplication/Controller.java
@@ -1,6 +1,25 @@
 package sample.runApplication;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import sample.runApplication.Main;
+
 public class Controller
 {
+    @FXML
+    private Pane main_pane;
+    @FXML private Button start_button;
+    @FXML private Button tasks_button;
 
+    @FXML private void initialize()
+    {
+        Main.setMainPane(main_pane);
+        /* Append the start page to the app */
+        Main.goToStartPage();
+
+        /* Set up the buttons to switch to their respective panes */
+        start_button.setOnAction(e -> Main.goToStartPage());
+        tasks_button.setOnAction(e -> Main.goToTasksPage());
+    }
 }
