cedc7b06dfcb58960d4fde81a8203f840641a1c6
Spotted Metal <38660687+spotted-metal@users.noreply.github.com>
2019-02-20 16:50:58 +0000

    Re-purpose main FXML controller
    - Remove obsolete FXML objects
    - Add functionality for the Moodle and BlueCastle buttons
    - Add a function to retrieve the tab pane (into which new tabs will be added to).
    
    Authors: Brendan

--- JavaFrontend_Project/src/sample/runApplication/ToolBarController.java
+++ JavaFrontend_Project/src/sample/runApplication/ToolBarController.java
@@ -2,23 +2,21 @@ package sample.runApplication;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.Pane;
 
 /*This is responsible for controlling the Tool bar, aka. fixed "Tasks" & "Start" buttons*/
 public class ToolBarController
 {
-    @FXML private Pane main_pane;
-    @FXML private Button start_button;
-    @FXML private Button tasks_button;
+    @FXML private TabPane tab_pane;
+    @FXML private Button open_moodle_button;
+    @FXML private Button blue_castle_button;
 
     @FXML private void initialize()
     {
-        Main.setMainPane(main_pane);
-        /* Append the start page to the app */
-        Main.goToStartPage();
-
-        /* Set up the buttons to switch to their respective panes */
-        start_button.setOnAction(e -> Main.goToStartPage());
-        tasks_button.setOnAction(e -> Main.goToTasksPage());
+        open_moodle_button.setOnAction(e -> Main.getApp().openTab("Moodle (courses)"));
+        blue_castle_button.setOnAction(e -> Main.getApp().openTab("Blue Castle (Grades)"));
     }
+
+    @FXML public TabPane getTabPane() { return tab_pane; }
 }
