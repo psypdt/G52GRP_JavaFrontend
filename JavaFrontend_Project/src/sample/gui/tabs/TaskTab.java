b35cd7af615f761882eaebc2e8fb59373c51d7df
Spotted Metal <38660687+spotted-metal@users.noreply.github.com>
2019-02-20 17:01:07 +0000

    Add buttons to allow switching views
    Add buttons to the TaskTab to allow switching between scraper and browser modes.
    
    Authors: Brendan

--- JavaFrontend_Project/src/sample/gui/tabs/TaskTab.java
+++ JavaFrontend_Project/src/sample/gui/tabs/TaskTab.java
@@ -1,6 +1,8 @@
 package sample.gui.tabs;
 
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
@@ -10,6 +12,8 @@ public class TaskTab extends Tab implements iTaskTab {
     private StackPane background;
     private Pane scraperView;
     private TabPane browserView;
+    private Button browserButton;
+    private Button scraperButton;
 
     private TaskTab() {
         super();
@@ -18,23 +22,32 @@ public class TaskTab extends Tab implements iTaskTab {
         scraperView = new StackPane();
         browserView = new TabPane();
 
+        browserButton = new Button("Go to browser view");
+        browserButton.setOnAction(e -> goToBrowserMode());
+
+        scraperButton = new Button("Go to scraper view");
+        scraperButton.setOnAction(e -> goToScraperMode());
+
         this.setContent(background);
     }
 
     public TaskTab(String id) {
         this();
         setText(id);
+        goToBrowserMode();
     }
 
     @Override
     public void goToScraperMode() {
         background.getChildren().clear();
         background.getChildren().add(scraperView);
+        background.getChildren().add(browserButton);
     }
 
     @Override
     public void goToBrowserMode() {
         background.getChildren().clear();
         background.getChildren().add(browserView);
+        background.getChildren().add(scraperButton);
     }
 }
