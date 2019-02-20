6292b59713a0245a76ba378a3f0fa297c8941a82
Philipp <psypdt@nottingham.ac.uk>
2019-02-20 15:52:38 +0000

    Allow user to send form to login to a site
    -This commit allows us to log into sites such as github or moodle.
    
    -NOTICE: We are not able to retrieve all the html at this time, since some of it is loaded after some script is executed, we need to find a way to let the page finish loading to obtain the missing info
    
    -Side effects: There should not be any side effects since everything is contained in the FormSender class
    
    Authors: Jay, Philipp

--- JavaFrontend_Project/src/sample/functionality/forms/formSending/FormSender.java
+++ JavaFrontend_Project/src/sample/functionality/forms/formSending/FormSender.java
@@ -1,18 +1,23 @@
 package sample.functionality.forms.formSending;
 
+import javafx.scene.web.WebView;
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import sample.functionality.parsing.parserWriter.ParserWriter;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.FormElement;
 
 import java.io.IOException;
+
 
 public class FormSender implements FormSenderInterface
 {
     private String url;
+    private static WebView webpage;
 
-    FormSender(String dest)
+    /*If this is added to something like a button or anything that can be "listened to", then it seems like it works*/
+    public FormSender(String dest)
     {
-        this.url = dest;
+
     }
 
 
@@ -23,32 +28,54 @@ public class FormSender implements FormSenderInterface
      * @return
      */
     @Override
-    public Document login(String userName, String password) throws IOException
+    public void login(String userName, String password) throws IOException
     {
-        Document document = Jsoup.connect(url)
-                .data("username", userName)
-                .data("password", password)
-                .userAgent("Mozilla")
-                .post();
+        /*Constants used in this example*/
+        final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36";
+        final String LOGIN_FORM_URL = "https://moodle.nottingham.ac.uk/login/index.php";
+        final String USERNAME = userName;
+        final String PASSWORD = password;
 
-        return document;
+        //Go to login page
+        Connection.Response loginFormResponse = Jsoup.connect(LOGIN_FORM_URL)
+                .method(Connection.Method.GET)
+                .userAgent(USER_AGENT)
+                .execute();
+
+        // Fill the login form
+        // Find the form first...
+        FormElement loginForm = (FormElement)loginFormResponse.parse()
+                .select("#login").first();
+        checkElement("Login Form", loginForm);
+
+        Element loginField = loginForm.select("#username").first();
+        checkElement("Login Field", loginField);
+        loginField.val(USERNAME);
+
+        Element passwordField = loginForm.select("#password").first();
+        checkElement("Password Field", passwordField);
+        passwordField.val(PASSWORD);
+
+
+        /*Send login form*/
+        Connection.Response loginActionResponse = loginForm.submit()
+                .cookies(loginFormResponse.cookies())
+                .userAgent(USER_AGENT)
+                .execute();
+
+        System.out.println(loginActionResponse.parse().html());
     }
 
 
-    public static void main(String[] args)
+    /***
+     * 
+     * @param name
+     * @param elem
+     */
+    public static void checkElement(String name, Element elem)
     {
-        FormSender formSender = new FormSender("https://moodle.nottingham.ac.uk/login/index.php");
-
-        try
-        {
-            Document m = formSender.login("psypdt", "");
-
-            ParserWriter parserWriter = new ParserWriter();
-            parserWriter.writeEntireHtmlToFile(m);
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
+        if (elem == null) {
+            throw new RuntimeException("Unable to find " + name);
         }
     }
 }
--- JavaFrontend_Project/src/sample/functionality/forms/formSending/FormSenderInterface.java
+++ JavaFrontend_Project/src/sample/functionality/forms/formSending/FormSenderInterface.java
@@ -1,11 +1,9 @@
 package sample.functionality.forms.formSending;
 
-import org.jsoup.nodes.Document;
-
 import java.io.IOException;
+
 
 public interface FormSenderInterface
 {
-    Document login(String userName, String password) throws IOException;
-
+    void login(String userName, String password) throws IOException;
 }
