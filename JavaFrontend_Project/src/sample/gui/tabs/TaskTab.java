36bc5bb78e5b12168e6fe685b7205a6610c9209a
Spotted Metal <38660687+spotted-metal@users.noreply.github.com>
2019-02-20 16:58:51 +0000

    Add basic functionality to TaskTab
    The TaskTab's are tabs that appear right of the Home tab on the top bar.
    They contain a scraper view (a Pane) and a browser view (a TabPane that will hold WebViewTab's).
    
    - Add ability to create an empty tab
    - Add functionality to the interface methods
    
    Authors: Brendan

--- JavaFrontend_Project/src/sample/gui/tabs/TaskTab.java
+++ JavaFrontend_Project/src/sample/gui/tabs/TaskTab.java
@@ -1,9 +1,40 @@
 package sample.gui.tabs;
 
-public interface TaskTab extends Tab {
+import javafx.geometry.Pos;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 
-    void close();
+public class TaskTab extends Tab implements iTaskTab {
 
-    void goToScraperMode();
-    void goToBrowserMode();
+    private StackPane background;
+    private Pane scraperView;
+    private TabPane browserView;
+
+    private TaskTab() {
+        super();
+
+        background = new StackPane();
+        scraperView = new StackPane();
+        browserView = new TabPane();
+
+        this.setContent(background);
+    }
+
+    public TaskTab(String id) {
+        this();
+        setText(id);
+    }
+
+    @Override
+    public void goToScraperMode() {
+        background.getChildren().clear();
+        background.getChildren().add(scraperView);
+    }
+
+    @Override
+    public void goToBrowserMode() {
+        background.getChildren().clear();
+        background.getChildren().add(browserView);
+    }
 }
