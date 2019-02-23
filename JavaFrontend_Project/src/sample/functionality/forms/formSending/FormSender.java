1ed7efa1c9f430c6332bf150bff4f6c8e0bb709c
Philipp <psypdt@nottingham.ac.uk>
2019-02-23 11:35:08 +0000

    Automatic login using WebEngine works
    This commit allows us to automatically login via the webengine.
    
    
    NOTE: We are still facing and issue, where the returned HTML is incomplete, this is because we have yet to figure out a way to confirm that the webpage has fully loaded.
    
    Side Effects: There is a dedicated class that is being used to run this, this class has been omitted. 
    
    Authors: Philipp

--- JavaFrontend_Project/src/sample/functionality/forms/formSending/FormSender.java
+++ JavaFrontend_Project/src/sample/functionality/forms/formSending/FormSender.java
@@ -1,25 +1,101 @@
 package sample.functionality.forms.formSending;
 
+import javafx.concurrent.Worker;
+import javafx.scene.control.Tab;
+import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.nodes.FormElement;
-
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.MalformedURLException;
 
 
-public class FormSender implements FormSenderInterface
+public class FormSender extends Tab implements FormSenderInterface
 {
     private String url;
-    private static WebView webpage;
+    public WebView webpage;
+    private int loginStates = 2; /*2 = form page loaded & sent the login form to server, 1 = got the final page, is it thread safe???*/
 
     /*If this is added to something like a button or anything that can be "listened to", then it seems like it works*/
     public FormSender(String dest)
     {
+        super();
+        webpage = new WebView();
+        webpage.getEngine().setJavaScriptEnabled(true);
 
+        webpage.getEngine().getLoadWorker().stateProperty().addListener((obs, oldValue, newValue) ->
+        {
+            System.out.println(newValue);
+            /*Make sure this is joined, otherwise the main finishes before this and kills the thread, hence it doesn't print*/
+            if (newValue == Worker.State.SUCCEEDED)
+            {
+                System.out.println(newValue);
+//                HTMLInputElement element = (HTMLInputElement) webpage.getEngine().getDocument().getElementsByTagName("body").item(0);
+//                element.click();
+
+                /*These method will keep being repeated in the background everytime the webengine is asked to load something new*/
+                if(loginStates == 2)
+                {
+                    try
+                    {
+                        login("psypdt", "");
+                    }
+                    catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+
+                if(loginStates == 1)
+                {
+                    tmp(webpage.getEngine());
+                }
+            }
+        });
+        System.out.println("Outside");
+        this.url = dest;
+        webpage.getEngine().load(url); /*After the tmp() runs the first time, this can be executed*/
     }
 
+
+
+    public void tmp(WebEngine webEngine)
+    {
+        org.w3c.dom.Document doc = webEngine.getDocument();
+
+        /*This prints everything to the command line*/
+        try {
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+            transformer.setOutputProperty(OutputKeys.METHOD, "html");
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+            transformer.transform(new DOMSource(doc),
+                    new StreamResult(new OutputStreamWriter(System.out, "UTF-8")));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+//        HTMLInputElement element = (HTMLInputElement) doc.getElementsByTagName("button").item(0);
+//        element.click();
+    }
+
+
+
+
+
+    /*================================================================================================================*/
 
     /***
      *
@@ -42,12 +118,12 @@ public class FormSender implements FormSenderInterface
                 .userAgent(USER_AGENT)
                 .execute();
 
-        // Fill the login form
-        // Find the form first...
-        FormElement loginForm = (FormElement)loginFormResponse.parse()
-                .select("#login").first();
+
+        /*Find the login from via its tag*/
+        FormElement loginForm = (FormElement)loginFormResponse.parse().select("#login").first();
         checkElement("Login Form", loginForm);
 
+        /*Complete the login form, user name & password*/
         Element loginField = loginForm.select("#username").first();
         checkElement("Login Field", loginField);
         loginField.val(USERNAME);
@@ -63,12 +139,21 @@ public class FormSender implements FormSenderInterface
                 .userAgent(USER_AGENT)
                 .execute();
 
-        System.out.println(loginActionResponse.parse().html());
+        System.out.println("Url after login was: " + loginActionResponse.url());
+        webpage.getEngine().load(loginActionResponse.url().toString());
+        loginStates--;
+        System.out.println("LoginStates is: " + loginStates);
+
+//        Document runp = Jsoup.connect("https://moodle.nottingham.ac.uk/theme/yui_combo.php?rollup/3.17.2/yui-moodlesimple-min.js")
+//                .ignoreContentType(true)
+//                .get();
+
+//        System.out.println(runp);
     }
 
 
     /***
-     * 
+     *
      * @param name
      * @param elem
      */
@@ -77,5 +162,21 @@ public class FormSender implements FormSenderInterface
         if (elem == null) {
             throw new RuntimeException("Unable to find " + name);
         }
+    }
+
+
+
+
+
+
+    public static void main(String args[])
+    {
+        FormSender formSender = new FormSender("https://moodle.nottingham.ac.uk/index.php");
+        try {
+            formSender.login("psypdt", "");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
