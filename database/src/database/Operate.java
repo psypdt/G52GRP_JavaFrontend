01bf9129e05f36d4f54887b1d21cd0f0cc7329d4
psyyw7 <psyyw7@nottingham.ac.uk>
2019-02-23 19:51:44 +0800

    translate some comments in the connection file and write the main function in other Operate file

---
+++ database/src/database/Operate.java
@@ -0,0 +1,18 @@
+package database;
+import java.sql.Connection;    
+import java.sql.DriverManager;    
+import java.sql.ResultSet;    
+import java.sql.SQLException;    
+import java.sql.Statement; 
+public class Operate {
+	static Connection conn;    
+	static Statement st; 
+	static Connection_test Op = new Connection_test();
+	public static void main(String[] args) {
+		Op.insert();   //insert data    
+        Op.update();   //update data    
+        Op.delete();   //delete data 
+        Op.query();    //Query records and display    
+	}
+
+}
