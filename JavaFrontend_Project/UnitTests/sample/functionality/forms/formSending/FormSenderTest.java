382f5cd6aca5ddb4a3b14ddf889ea0d9d21d71e9
Philipp <psypdt@nottingham.ac.uk>
2019-02-27 11:59:19 +0000

    Add JUnit tests for FormSender class
    This commit will continue adding JUnit tests for the FormSender class. There are some comments regarding how methods such as login() will need to change in the future.
    
    Side effects: There should be no side effects since only the FormSender Testing class was modified.
    
    Authors: Philipp, Jay

--- JavaFrontend_Project/UnitTests/sample/functionality/forms/formSending/FormSenderTest.java
+++ JavaFrontend_Project/UnitTests/sample/functionality/forms/formSending/FormSenderTest.java
@@ -1,28 +1,52 @@
 package sample.functionality.forms.formSending;
 
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.FormElement;
 import org.junit.Test;
-
 import java.io.IOException;
-import java.text.Normalizer;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
-public class FormSenderTest {
+
+public class FormSenderTest
+{
+    /*We can debate if this should be the way we handle an invalid input, maybe we have a method to change the URL?*/
+    @Test
+    public void blank_constructor_arg()
+    {
+        try
+        {
+            FormSender formSender = new FormSender("");
+        }
+        catch (ExceptionInInitializerError e)
+        {
+            assertThat(e.getMessage(), is("Error, destination URL is required"));
+        }
+    }
+
 
     @Test
-    public void printHtmlToConsole() {
+    public void create_FormSender_obj()
+    {
+        FormSender formSender = new FormSender("https://moodle.nottingham.ac.uk/login/index.php");
+        assertNotNull(formSender);
+    }
+
+
+    /*Currently not used*/
+    @Test
+    public void printHtmlToConsole()
+    {
 
     }
 
+
     @Test
-    public void login_incorrect_password() {
+    public void login_incorrect_password()
+    {
         String username = "psyjm9";
-        String password = "asdf";
+        String password = "notMyPassword";
         FormSender form = new FormSender("https://moodle.nottingham.ac.uk/login/index.php");
 
         try
@@ -36,9 +60,11 @@ public class FormSenderTest {
         }
     }
 
+
     @Test
-    public void login_incorrect_username() {
-        String username = "asdf";
+    public void login_incorrect_username()
+    {
+        String username = "NotMyUsername";
         String password = "ThisIsMyPassword";
         FormSender form = new FormSender("https://moodle.nottingham.ac.uk/login/index.php");
 
@@ -53,9 +79,12 @@ public class FormSenderTest {
         }
     }
 
+
+    /*Note that the login method will need to call the parsing class and return JSON, this needs to be amended*/
     @Test
-    public void login_no_password() {
-        String username = "asdf";
+    public void login_no_password()
+    {
+        String username = "psypdt";
         String password = "";
         FormSender form = new FormSender("https://moodle.nottingham.ac.uk/login/index.php");
 
@@ -70,8 +99,10 @@ public class FormSenderTest {
         }
     }
 
+
     @Test
-    public void login_no_username() {
+    public void login_no_username()
+    {
         String username = "";
         String password = "ThisIsMyPassword";
         FormSender form = new FormSender("https://moodle.nottingham.ac.uk/login/index.php");
@@ -87,10 +118,10 @@ public class FormSenderTest {
         }
     }
 
+
     @Test
     public void check_non_existent_Element()
     {
-
         String element_name = "#Invalid_login_name";
         try
         {
@@ -113,11 +144,12 @@ public class FormSenderTest {
         }
     }
 
+
     @Test
     public void check_existent_Element()
     {
-
         String element_name = "#login";
+
         try
         {
             FormSender form = new FormSender("https://moodle.nottingham.ac.uk/login/index.php");
@@ -138,6 +170,33 @@ public class FormSenderTest {
         }
     }
 
+
+    @Test
+    public void check_empty_Element()
+    {
+        String element_name = "";
+
+        try
+        {
+            FormSender form = new FormSender("https://moodle.nottingham.ac.uk/login/index.php");
+            try
+            {
+                Document moodle = Jsoup.connect("https://moodle.nottingham.ac.uk/login/index.php").get();
+                FormElement loginForm = moodle.select(element_name).forms().get(0);
+                form.checkElement(element_name, loginForm);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        catch (RuntimeException e)
+        {
+            assertThat(e.getMessage(), is("Empty Tag is an illegal input"));
+        }
+    }
+
+
     @Test
     public void fetchUsrName()
     {
@@ -147,17 +206,34 @@ public class FormSenderTest {
         assertEquals(username, form.fetchUsrName(username));
     }
 
+
+    /*This method and teh fetchUsrName method should be revised, make sure that they actually do what we want*/
     @Test
-    public void fetchPassWord() {
+    public void fetchPassWord()
+    {
         FormSender form = new FormSender("https://moodle.nottingham.ac.uk/login/index.php");
-        String password = "asdf";
+        String password = "ThePassword";
 
         assertEquals(password, form.fetchUsrName(password));
     }
 
     @Test
-    public void getWebView() {
+    public void fetchUsername()
+    {
+        FormSender form =  new FormSender("https://moodle.nottingham.ac.uk/login/index.php");
+        String username = "TheUsername";
+
+        assertEquals(username, form.fetchUsrName(username));
+    }
+
+
+    @Test
+    public void getWebView()
+    {
         FormSender form = new FormSender("https://moodle.nottingham.ac.uk/login/index.php");
         assertNotNull(form.getWebView());
     }
+
+
+
 }
