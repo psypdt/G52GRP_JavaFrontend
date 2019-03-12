22ab05ce788f862ad31035d215f1f2285aef02b4
Spotted Metal <38660687+spotted-metal@users.noreply.github.com>
2019-03-12 13:24:17 +0000

    Add polling behaviour
    Make the helper task repeatedly request for the form, so that it can be found regardless of how fast/slow the webpage loads.

--- JavaFrontend_Project/src/sample/functionality/forms/formSending/LoginToMyNottingham.java
+++ JavaFrontend_Project/src/sample/functionality/forms/formSending/LoginToMyNottingham.java
@@ -24,8 +24,8 @@ class AutomateMyNottinghamLogin extends Task {
     /**
      * This is the constructor for the {@code AutomateMyNottinghamLogin} class
      * @param engine The {@code WebEngine} that is being used to display the sites
-     * @param username The Username, should be changed asap (shouldn't be plane text)
-     * @param password The Password, should be changed asap (shouldn't be plane text)
+     * @param username The Username, should be changed asap (shouldn't be plain text)
+     * @param password The Password, should be changed asap (shouldn't be plain text)
      */
     AutomateMyNottinghamLogin(WebEngine engine, String username, String password) {
         this.engine = engine;
@@ -39,17 +39,40 @@ class AutomateMyNottinghamLogin extends Task {
 
         Element doc = engine.getDocument().getDocumentElement();
 
-        /* compare and contrast with the following JavaScript code:
+        // HTML elements
+        Element doc;
+        NodeList forms;
+        HTMLFormElement loginForm = null;
+
+        /* Compare and contrast with the following JavaScript code:
          *
          *   let forms = document.getElementsByTagName("form");
          *   let loginForm = forms[1];
+         *
          *   let inputs = loginForm.elements;
          *   inputs[2].value = username;
          *   inputs[3].value = password;
          *   loginForm.submit();
          */
-        NodeList forms = doc.getElementsByTagName("form");
-        HTMLFormElement loginForm = (HTMLFormElement)forms.item(1);
+
+        do {  // keep polling for the form
+
+            System.out.println("Finding the form...");
+            try { Thread.sleep(millisToWait); } catch (Exception e) { e.printStackTrace(); }
+
+            if (engine.getDocument() != null) {
+                doc = engine.getDocument().getDocumentElement();
+                forms = doc.getElementsByTagName("form");
+                loginForm = (HTMLFormElement)forms.item(1);
+            }
+
+            //millisToWait *= 2;  // exponential back-off
+            millisToWait += 100;  // incremental back-off
+
+        } while (loginForm == null);
+
+        System.out.println("Found it!");
+
         HTMLCollection inputs = loginForm.getElements();
         ((HTMLInputElement)inputs.item(2)).setValue(username);
         ((HTMLInputElement)inputs.item(3)).setValue(password);
