5d6853ec5c701af675f9eeff89ff652e72100ac5
Spotted Metal <38660687+spotted-metal@users.noreply.github.com>
2019-03-20 16:48:15 +0000

    Refactor Application screen files
    Add FXML IDs to all buttons for future implementation. Expand comments

--- JavaFrontend_Project/src/sample/runApplication/ApplicationController.java
+++ JavaFrontend_Project/src/sample/runApplication/ApplicationController.java
@@ -5,26 +5,31 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TabPane;
 
 /**
- * This is responsible for controlling the Tool bar, aka. fixed "Tasks" & "Start" buttons
+ * This class is responsible for setting up the functionality of the buttons on the home screen.
  */
 public class ApplicationController
 {
     @FXML private TabPane tab_pane;
-    @FXML private Button open_moodle_button;
-    @FXML private Button blue_castle_button;
+    @FXML private Button    button_moodle,
+                            button_portal,
+                            button_grades_modules,
+                            button_blue_castle,
+                            button_mynottingham;
+    @FXML private Button load_database;
+    @FXML private Button button_settings;
 
     /**
-     * Opening the tab by using the if that we set
+     * Hook up functionality to the buttons on the home screen.
      */
     @FXML private void initialize()
     {
-        open_moodle_button.setOnAction(e -> Main.getGuiHandler().openTab("Moodle (courses)"));
-        blue_castle_button.setOnAction(e -> Main.getGuiHandler().openTab("Blue Castle (Grades)"));
+        button_moodle.setOnAction(e -> Main.getGuiHandler().openTab("Moodle (courses)"));
+        button_blue_castle.setOnAction(e -> Main.getGuiHandler().openTab("Blue Castle (Grades)"));
     }
 
     /**
-     * To generate Tap Pane
-     * @return
+     * Get a reference to the tab pane to allow for the opening and closing of tabs.
+     * @return The primary tab pane on the screen.
      */
     @FXML public TabPane getTabPane() { return tab_pane; }
 }
