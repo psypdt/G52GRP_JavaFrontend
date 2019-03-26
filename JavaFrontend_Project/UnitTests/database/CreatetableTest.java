d99bb81bcd76a908d1a7445506a816048591249b
psyyw7 <psyyw7@nottingham.ac.uk>
2019-03-26 20:31:04 +0800

    finish the CreateTable class test

---
+++ JavaFrontend_Project/UnitTests/database/CreatetableTest.java
@@ -0,0 +1,32 @@
+package database;
+import database.CreateTable;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+public class CreatetableTest {
+	public static Connection getConnection() {    
+        Connection con = null;  //Create a Connection object to connect to the database 
+        try {    
+            Class.forName("com.mysql.cj.jdbc.Driver");// load Mysql driver   
+                
+            con = DriverManager.getConnection(    
+                    "jdbc:mysql://localhost:3306/database", "root", "sh279000");// create database connection  
+                
+        } catch (Exception e) {    
+            System.out.println("connect to the database fail" + e.getMessage());    
+        }    
+        return con; //Returns the established database connection
+    }
+	@Test
+	public void test() {
+		CreateTable create = new CreateTable();
+		String testresult = create.createtable("newnew");
+		assertEquals("newnew",testresult);
+	}
+
+}
