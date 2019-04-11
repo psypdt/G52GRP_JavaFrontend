03a51786669fb4bc070d509b78a5d98cedbfd935
Philipp <psypdt@nottingham.ac.uk>
2019-04-11 19:22:26 +0100

    Continue standardising code
    - Add JavaDocs & reformat how comments are done

--- JavaFrontend_Project/src/sample/functionality/parsing/parser/JSONParser.java
+++ JavaFrontend_Project/src/sample/functionality/parsing/parser/JSONParser.java
@@ -15,25 +15,27 @@ import java.util.Map;
 public class JSONParser implements ParserInterface
 {
     private String m_Url;
-    private Document m_Document; /*Stores the HTML that will be parsed temporarily*/
-    private FormSender m_FormSender; /*FormSender allows user login to occur right before parsing, without state change*/
+    private Document m_Document; //Stores the HTML that will be parsed temporarily.
+    private FormSender m_FormSender; //FormSender allows user login to occur right before parsing, without state change.
 
     /**
-     * Constructor for {@link JSONParser}
+     * @implNote Currently the constructor does nothing since there is no need for it do execute anything.
+     * Constructor for {@link JSONParser}.
      */
     public JSONParser()
     {
     }
 
     /**
-     * This method is used to log the user into a website, and then parses a set of tags after the m_FormSender is complete
-     * @param loginUrl Url to the website where the m_FormSender form is located
-     * @param tags The tag that will be parsed after the m_FormSender is completed
-     * @param username The users username for the website
-     * @param password The users password for the website
-     * @param loginTags The tags required to find the m_FormSender form
-     * @param isStatic Specify if the website dynamically generates its login form, or if it is static
-     * @return {@code JSONArray pageAsString}, this is a JSONArray that contains the parsed information
+     * This method is used to log the user into a website, and then parses a set of specified tags.
+     * after the {@code m_FormSender} has completed the login process.
+     * @param loginUrl Url to the website where the m_FormSender form is located.
+     * @param tags The tag that will be parsed after the m_FormSender is completed.
+     * @param username The users username for the website.
+     * @param password The users password for the website.
+     * @param loginTags The tags required to find the m_FormSender form.
+     * @param isStatic Specify if the website dynamically generates its login form, or if it is static.
+     * @return {@code JSONArray pageAsString}, this is a JSONArray that contains the parsed information.
      */
     @Override
     public JSONArray login(String loginUrl, String tags, String username, String password, ArrayList<String> loginTags, boolean isStatic)
@@ -42,6 +44,7 @@ public class JSONParser implements ParserInterface
         Map<String, String> loginCookies = null;
         m_FormSender = new FormSender(m_Url, isStatic, username, password, loginTags); //Currently only static works
         //m_FormSender.staticFormLogin(username, password);
+
         while(loginCookies == null)
         {
             System.out.println("a");
@@ -51,29 +54,29 @@ public class JSONParser implements ParserInterface
 
         m_Url = m_FormSender.getWebView().getEngine().getLocation();
 
-        //DEBUG:
-        System.out.println(m_Url);
+        //DEBUG: Print current URL.
+        //System.out.println(m_Url);
 
         JSONArray pageAsString = new JSONArray();
 
-        try
+        try //Try connecting to the current website to get its contents.
         {
+            //Save the website contents into m_Document.
             m_Document = Jsoup.connect(m_Url)
                     .cookies(loginCookies)
                     .get();
 
             pageAsString = this.getJSONArray(tags);
-        } catch (Exception e) {
-            e.printStackTrace();
         }
+        catch (Exception e) { e.printStackTrace(); }
 
         return pageAsString;
     }
 
 
     /**
-     * Getter for the web-site "{@code m_Url}" HTML
-     * @return Returns the HTML that was fetched by the background thread
+     * Getter for the web-site "{@code m_Url}" HTML.
+     * @return Returns the HTML that was fetched by the background thread.
      */
     public Document getDocument() {
         return m_Document;
@@ -81,19 +84,20 @@ public class JSONParser implements ParserInterface
 
 
     /**
-     * This is used in the {@code JSONParser.m_FormSender(...)} method, this method constructs a JSONArray and populates it
-     * @param cssQuery This is the tag that will be used to parse the m_Document (contains HTML of website)
-     * @return {@code JSONArray} that contains the parsed tags
+     * This is used in the {@code JSONParser.m_FormSender(...)} method. Method constructs & populates a JSONArray.
+     * @param cssQuery This is the tag that will be used to parse the m_Document (contains HTML of website).
+     * @return {@code JSONArray} that contains the parsed tags.
      */
-    private JSONArray getJSONArray(String cssQuery) {
+    private JSONArray getJSONArray(String cssQuery)
+    {
         JSONArray parsedTagJsonArray = new JSONArray();
-
-        Elements elements = m_Document.select(cssQuery); /*Elements that are of interest*/
+        Elements elements = m_Document.select(cssQuery); //Elements that are of interest.
 
         for (Element e : elements)
         {
             //DEBUG: Prints the element found, used to see if correct element was found
-            System.out.println("Element: " + e.tagName() + ", Text: " + e.ownText());
+            //System.out.println("Element: " + e.tagName() + ", Text: " + e.ownText());
+
             parsedTagJsonArray.put(parseElement(e));
         }
 
@@ -103,45 +107,51 @@ public class JSONParser implements ParserInterface
 
 
     /**
-     * @implNote If nesting is too deep (more than 20 levels) or if object starts at the incorrect place, accessing element out of range
-     * This is a recursive method. The method populates the JSON fields, meaning it fills in the values the parsed tags
-     * This method is used by {@code getJSONArray(String cssQuery)}
-     * @param element The element that will be explored (check if {@code element} has children that should be parsed)
+     * @implNote If tag nesting is too deep (more than 20 levels, restriction place by {@link Jsoup})
+     * or if object starts at the incorrect place (accessing element out of range) then a crash can occur.
+     *@implNote This is a recursive method. The method populates the JSON fields, ie. fills in values of parsed tags.
+     * This method is used by {@code getJSONArray(String cssQuery)}.
+     * @param element The element that will be explored (check if {@code element} has children that should be parsed).
+     * @return {@code JSONObject} representing the {@code element} and all its children.
      */
     private JSONObject parseElement(Element element)
     {
-        JSONObject obj = new JSONObject();
+        JSONObject jsonParsedTag = new JSONObject();
 
-        //This constructs the JSONObject
+        //This constructs the JSONObject.
         try
         {
-            obj.put("type", element.tagName());
+            jsonParsedTag.put("type", element.tagName());
 
             if (!element.ownText().equals(""))
             {
+                //DEBUG: Print the tag name and tag contents.
                 System.out.println("Element: " + element.tagName() + ", Text: " + element.ownText());
-                obj.put("text", element.ownText());
+                jsonParsedTag.put("text", element.ownText());
             }
 
             if (element.tagName().equals("a") && element.attributes().get("href") != null)
             {
-                obj.put("href", element.attributes().get("href"));
+                jsonParsedTag.put("href", element.attributes().get("href"));
             }
 
-            if (element.children().size() > 0)
+            if (element.children().size() > 0) //Check if tag has children.
             {
-                //DEBUG: Used to show that a child has been correctly added to the JSONObject
+                //DEBUG: Used to show that a child has been correctly added to the JSONObject.
                 System.out.println(element.children().toString());
 
                 JSONArray childrenArray = new JSONArray();
 
-                /*For every child do a recursive function call*/
-                for (Element child : element.children()) { childrenArray.put(parseElement(child)); }
+                //For every child (should it exist) do a recursive function call.
+                for (Element child : element.children())
+                {
+                    childrenArray.put(parseElement(child));
+                }
 
-                obj.put("children", childrenArray);
+                jsonParsedTag.put("children", childrenArray);
             }
         } catch (JSONException e) { e.printStackTrace(); }
 
-        return obj;
+        return jsonParsedTag;
     }
 }
--- JavaFrontend_Project/src/sample/functionality/parsing/parserReader/ParserReader.java
+++ JavaFrontend_Project/src/sample/functionality/parsing/parserReader/ParserReader.java
@@ -5,26 +5,30 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-
-//NOTE: This class is not in use, this class allows Tags to be read from a file
+/**
+ * @implNote This class is not in use but has been implemented, this class allows Tags to be read from a file.
+ */
 public class ParserReader implements ParserReaderInterface
 {
+    /**
+     * Constructor for {@link ParserReader}.
+     */
     public ParserReader(){}
 
     /***
      * This function will read the file containing the parsed site data.
-     * NOTE: Not sure if this should return a String or something else, reason, returning a large string is inefficient
-     * @param filePath Location of file that contains parsed data
-     * @throws IOException Thrown due to BufferReader(), thrown if file not found
+     * @implSpec Developer may wish to return something other than a {@code ArrayList<String>}, do this via a new method.
+     * @param filePath Location of file that contains parsed data.
+     * @throws IOException {@code BufferReader()} can throw {@code IOException}, thrown if file not found.
      */
     @Override
     public ArrayList<String> readParsedFile(String filePath) throws IOException
     {
+        String fileLine; //A single line in the file.
         ArrayList <String> fileContents = new ArrayList<>();
-        String fileLine; /*A single line in the file*/
         BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
 
-        /*Reads the file into a string */
+        //Reads the file into a string, tags can be extracted from here.
         while((fileLine = bufferedReader.readLine()) != null)
         {
             fileContents.add(fileLine);
