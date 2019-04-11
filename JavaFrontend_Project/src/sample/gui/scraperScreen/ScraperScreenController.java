psypdt/G52GRP_JavaFrontend

commit ba3aae4064163957711f2b8bb96bbf9d97aeb88d

Modify login screen FXML
CHANGES:
* Redo layout of input elements to allow proper scaling behaviour (centered in screen while re-sizing)
* Re-arrange input elements to establish proper tabbing behaviour

ISSUES:
* Re-sizing the window to its minimum size still causes the login form to be displayed weirdly

Authors: Brendan, Chris

JavaFrontend_Project/src/sample/gui/scraperScreen/ScraperscreenView.fxml
@@ -5,31 +5,32 @@
 <?import javafx.scene.control.PasswordField?>
 <?import javafx.scene.control.TextField?>
 <?import javafx.scene.layout.AnchorPane?>
+<?import javafx.scene.layout.StackPane?>
 <?import javafx.scene.text.Font?>
 
-<AnchorPane fx:id="pane" xmlns="http://javafx.com/javafx/8.0.172-ea" xmlns:fx="http://javafx.com/fxml/1" fx:controller="sample.gui.scraperScreen.ScraperScreenController">
+<StackPane maxHeight="1.7976931348623157E308" maxWidth="1.7976931348623157E308" style="-fx-background-color: #B7E2F0;" xmlns="http://javafx.com/javafx/8.0.172-ea" xmlns:fx="http://javafx.com/fxml/1" fx:controller="sample.gui.scraperScreen.ScraperScreenController">
    <children>
-      <AnchorPane prefHeight="400.0" prefWidth="600.0" style="-fx-background-color: #B7E2F0;">
+      <AnchorPane maxHeight="-Infinity" maxWidth="-Infinity" minHeight="-Infinity" minWidth="-Infinity" prefHeight="200.0" prefWidth="300.0" StackPane.alignment="CENTER">
          <children>
-            <Button fx:id="login_button" layoutX="297.0" layoutY="284.0" mnemonicParsing="false" style="-fx-background-color: #0000A0;" text="login" textFill="WHITE" />
-            <PasswordField fx:id="password_field" layoutX="243.0" layoutY="235.0" />
-            <TextField fx:id="username_field" layoutX="243.0" layoutY="187.0" />
-            <Label layoutX="140.0" layoutY="189.0" style="-fx-background-color: E9FFFF;" text="Username" textFill="#2153bf">
+            <Label alignment="CENTER" prefWidth="300.0" text="Login Page" textAlignment="CENTER" textFill="#a18b34" textOverrun="CLIP">
+               <font>
+                  <Font size="48.0" />
+               </font>
+            </Label>
+            <Label layoutX="25.0" layoutY="90.0" style="-fx-background-color: E9FFFF;" text="Username" textFill="#2153bf">
                <font>
                   <Font name="System Bold" size="18.0" />
                </font>
             </Label>
-            <Label layoutX="142.0" layoutY="237.0" style="-fx-background-color: E9FFFF;" text="Password" textFill="#2153bf">
+            <TextField fx:id="username_field" layoutX="125.0" layoutY="90.0" prefWidth="150.0" />
+            <Label layoutX="25.0" layoutY="130.0" style="-fx-background-color: E9FFFF;" text="Password" textFill="#2153bf">
                <font>
                   <Font name="System Bold" size="18.0" />
                </font>
             </Label>
-            <Label layoutX="140.0" layoutY="36.0" text="Login Page" textFill="#a18b34">
-               <font>
-                  <Font name="Charter Bold" size="62.0" />
-               </font>
-            </Label>
+            <PasswordField fx:id="password_field" layoutX="125.0" layoutY="130.0" prefWidth="150.0" />
+            <Button fx:id="login_button" layoutX="150.0" layoutY="170.0" mnemonicParsing="false" style="-fx-background-color: #0000A0;" text="login" textFill="WHITE" />
          </children>
       </AnchorPane>
    </children>
-</AnchorPane>
+</StackPane>

commit 883f577d1c5c62bac81fdd22f580e03f44befcfc

Fix login screen functionality
Fix bug introduced in ba3aae4064163957711f2b8bb96bbf9d97aeb88d, where the AnchorPane was not properly replaced with the StackPane.

JavaFrontend_Project/src/sample/gui/scraperScreen/ScraperScreenController.java
@@ -6,7 +6,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -21,7 +20,7 @@ public class ScraperScreenController
 {
     private String m_Id;
     private ArrayList<String> m_LoginTags = new ArrayList<>();
-    @FXML private AnchorPane anchor_pane;
+    @FXML private Pane background_pane;
     @FXML private VBox vertical_grid;
     @FXML private TextField username_field;
     @FXML private PasswordField password_field;
@@ -95,8 +94,8 @@ public class ScraperScreenController
         vertical_grid.setLayoutY(100);
 
         // remove the login form and put the primary container into the view
-        anchor_pane.getChildren().clear();
-        anchor_pane.getChildren().add(vertical_grid);
+        background_pane.getChildren().clear();
+        background_pane.getChildren().add(vertical_grid);
 
         // iterate through the JSON array and display each element
         for (int i = 0; i < elements.length(); i++)

JavaFrontend_Project/src/sample/gui/scraperScreen/ScraperscreenView.fxml
@@ -8,7 +8,7 @@
 <?import javafx.scene.layout.StackPane?>
 <?import javafx.scene.text.Font?>
 
-<StackPane maxHeight="1.7976931348623157E308" maxWidth="1.7976931348623157E308" style="-fx-background-color: #B7E2F0;" xmlns="http://javafx.com/javafx/8.0.172-ea" xmlns:fx="http://javafx.com/fxml/1" fx:controller="sample.gui.scraperScreen.ScraperScreenController">
+<StackPane fx:id="background_pane" maxHeight="1.7976931348623157E308" maxWidth="1.7976931348623157E308" style="-fx-background-color: #B7E2F0;" xmlns="http://javafx.com/javafx/8.0.172-ea" xmlns:fx="http://javafx.com/fxml/1" fx:controller="sample.gui.scraperScreen.ScraperScreenController">
    <children>
       <AnchorPane maxHeight="-Infinity" maxWidth="-Infinity" minHeight="-Infinity" minWidth="-Infinity" prefHeight="200.0" prefWidth="300.0" StackPane.alignment="CENTER">
          <children>