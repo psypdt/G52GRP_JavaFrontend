fa5e1a1aa25cced89a9877cae0ab3a38cdc99759
Philipp <psypdt@nottingham.ac.uk>
2019-04-13 15:47:08 +0100

    Add JavaDoc to User class

--- JavaFrontend_Project/src/database/User.java
+++ JavaFrontend_Project/src/database/User.java
@@ -6,6 +6,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+
+
 /**
  *@implNote Any method that connects to a database MUST call {@link #initialiseConnection()} as its first operation.
  *@implNote Any method that connects to a database MUST call {@link Connection#close()} before returning.
@@ -17,32 +19,39 @@ public class User extends DatabaseManipulator
     private final String m_Fallback = ""; // Safety measure, sent if query doesn't adhere to standard.
 
 
+
     /**
-     * Constructor for {@link User} class.
+     * Constructor for the {@link User} class.
      * @implSpec By default the constructor does not initialize {@link #m_Connection}, all it does is call {@code super()}.
      */
     public User() { super(); }
 
 
 
+
     /**
-     * @implSpec This method
+     * @implSpec This method inserts a new student into the database.
      * @param lastName Students last name. Constraint: Can't be an empty {@link String}.
      * @param firstName Students first name. Constraint: Can't be an empty  {@link String}.
      * @param userID Student's ID. Constraint: ID must be greater than 0, {@code userID > 0}.
      */
     public void insert(String lastName, String firstName, int userID)
     {
-        m_Connection = initialiseConnection(); // The first step is to get a connection, that is, to the database
+        m_Connection = initialiseConnection(); // Initialize connection to database as specified by super class.
+        String sqlQuery = m_Fallback;
+
+
+        if(!lastName.isEmpty() && !firstName.isEmpty() && userID >0)
+        {
+             sqlQuery = "INSERT INTO user(Lastname, Firstname, UserID)"
+                    + " VALUES ('"+lastName+"','"+firstName+"','"+userID+"')";  // SQL statement to insert data
+        }
 
         try
         {
-            String sql = "INSERT INTO user(Lastname, Firstname, UserID)"
-                    + " VALUES ('"+lastName+"','"+firstName+"','"+userID+"')";  // SQL statement to insert data
-
-            m_Statement = m_Connection.createStatement();    // Create a Statement object for executing static SQL statements
-            m_Statement.executeUpdate(sql);  // SQL statement that performs the insert operation and returns the number of inserted data
-            m_Connection.close();   //Close the database connection
+            m_Statement = m_Connection.createStatement(); // Create a Statement object to execute the SQL statement.
+            m_Statement.executeUpdate(sqlQuery); // SQL statement that performs the insert operation.
+            m_Connection.close(); // Close the database connection as specified by the super class.
         }
         catch (SQLException e)
         {
@@ -52,40 +61,69 @@ public class User extends DatabaseManipulator
 
 
     /**
-     *
-     * @param firstname user's first name
-     * @param userID user's ID
+     * @implSpec The {@link #m_Fallback} query will be executed of parameter constrains are violated.
+     * @param firstName The new firstName given to the student. Constraint: Can't be an empty {@link String}.
+     * @param userID The ID of the student who's name will be changed. Constraint: Must be greater than 0 {@code userID > 0}.
      */
-    public void update(String firstname , int userID) {
-        m_Connection = initialiseConnection(); //Again, get the connection first, that is, connect to the database
-        try {
-            String sql = "update user set UserID = '"+userID+"' where Firstname ='"+firstname+"'";// SQL statement that updates data
-            m_Statement = m_Connection.createStatement();    //Create a Statement object for executing static SQL statements, m_Statement being a local variable
-            m_Statement.executeUpdate(sql);//SQL statement that performs the update operation and returns the number of updates
-            m_Connection.close();   //Close the database connection
-        } catch (SQLException e) {
+    public void update(String firstName , int userID)
+    {
+        m_Connection = initialiseConnection(); //Create connection to database.
+        String sqlQuery = m_Fallback;
+
+        if(!firstName.isEmpty() && userID > 0)
+        {
+            sqlQuery = "update user set UserID = '"+userID+"' where Firstname ='"+firstName+"'";// SQL updates statement.
+        }
+
+        try
+        {
+            m_Statement = m_Connection.createStatement(); //Create a Statement that will execute the update query.
+            m_Statement.executeUpdate(sqlQuery);// SQL statement that performs the update operation.
+            m_Connection.close(); // Close the database connection as specified by the super class.
+        }
+        catch (SQLException e)
+        {
             System.out.println("update fail");
         }
     }
-    public String query() {
-        String searchresult =null;
-        m_Connection = initialiseConnection(); //Again, get the connection first, that is, connect to the database
-        try {
-            String sql1 = "select * from user";// SQL statements that query data
-            m_Statement = m_Connection.createStatement();    //Create a Statement object for executing static SQL statements, m_Statement being a local variable
-            ResultSet rs1 = m_Statement.executeQuery(sql1);  //Execute the SQL query statement and return the result set of the query data
-            System.out.println("search result：");
-            while (rs1.next()) {
-                String Ln = rs1.getString("Lastname");
-                String Fn = rs1.getString("Firstname");
-                String UID = rs1.getString("UserID");
-                searchresult = Fn+" "+Ln + " " + UID;
-                System.out.println(searchresult);
+
+
+    /**
+     *
+     * @return {@link String} containing all the users in the {@code user} table in the database.
+     */
+    public String query()
+    {
+        m_Connection = initialiseConnection(); // Initialize connection to database as specified by super class.
+        String searchResult = null;
+
+        try
+        {
+            String sqlQuery = "select * from user";// SQL statements that query data
+
+            m_Statement = m_Connection.createStatement(); // Create a Statement object to execute the sql query.
+            ResultSet resultSet = m_Statement.executeQuery(sqlQuery); // Execute SQL query statement and save the returned data.
+
+            //DEBUG: View what the query returned.
+            //System.out.println("search result：");
+
+            while (resultSet.next())
+            {
+                String Ln = resultSet.getString("Lastname");
+                String Fn = resultSet.getString("Firstname");
+                String UID = resultSet.getString("UserID");
+                searchResult = Fn+" "+Ln + " " + UID;
+
+                //DEBUG: View what the search result was.
+                //System.out.println(searchresult);
             }
-            m_Connection.close();   //Close the database connection
-            return searchresult;
-        } catch (SQLException e) {
-            System.out.println("search information fail");
+            m_Connection.close(); // Close the database connection as specified by the super class.
+
+            return searchResult;
+        }
+        catch (SQLException e)
+        {
+            System.out.println("Failed to query user table " + e.getMessage());
         }
         return null;
     }
